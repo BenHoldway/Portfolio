5f2851da0af8e66339e5e1ef6305887cec79fd86
BenHoldway03 <benholdway03@gmail.com>
2024-09-08 22:34:59 +0100

    Sizing Updates

--- main.js
+++ main.js
@@ -1,88 +1,54 @@
-var projectSelected = 'Games';
-localStorage.setItem('projectSelected', projectSelected);
+$(document).ready(function(){
+
+  ChangeHeader();
+  $(window).on('resize', function () {
+    ChangeHeader();
+  });
+
+  if($(".ProjectContainer").length > 0) ChangeCategory("All");
+
+  $("#CompressedIconToggle").on("click", function(){
+    $("#NavLinks").toggle();
+  });
+
+  $(".ProjectButton").on("click", function(){
+    ChangeCategory($(this).attr("id").replace("Button", ""));
+  })
+});
+
+
 
 
 function ChangeHeader()
 {
   if(window.innerWidth <= 980)
   {
-    document.getElementById("HeaderFull").classList.add("HeaderHidden")
-    document.getElementById("HeaderCompressed").classList.remove("HeaderHidden");
+    $("#HeaderFull").addClass("HeaderHidden")
+    $("#HeaderCompressed").removeClass("HeaderHidden");
   }
   else
   {
-    document.getElementById("HeaderFull").classList.remove("HeaderHidden");
-    document.getElementById("HeaderCompressed").classList.add("HeaderHidden");
+    $("#HeaderFull").removeClass("HeaderHidden");
+    $("#HeaderCompressed").addClass("HeaderHidden");
+    $("#NavLinks").hide();
   }
 }
 
-function MobileNavSelect() {
-  var x = document.getElementById("NavLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+function ChangeCategory(category) {
+  $('.ProjectContainer').hide();
+
+  if(category === 'All') $('.ProjectContainer').show();
+  else $('.' + category).show();
+
+  $('.ProjectButton').addClass('CategoryActive').not('#' + category + 'Button').removeClass('CategoryActive');
+
+  sessionStorage.Category = category;
+  console.log(sessionStorage.Category);
 }
 
-function ChangeProjectVisibility(projectType) {
-  var games = document.getElementById("Games");
-  var other = document.getElementById("Other");
-  
-  /*if (projectType == 'Games' && (games.style.opacity > 0 || other.style.opacity < 1))  
-  {
-    if(other.style.display == block)
-      other.style.opacity += 0.1;
 
-    games.style.opacity -= 0.1;
 
-    if(games.style.opacity == 0)
-    {
-      games.style.display = none;
-      other.style.display = block;
-      other.style.opacity += 0.1;
-    }
-
-    setTimeout(function(){ChangeProjectVisibility(projectType)}, 2000);
-  } 
-  else if (projectType == 'Other' && (other.style.opacity > 0 || games.style.opacity < 1))  
-  {
-    if(games.style.display == block)
-      games.style.opacity += 0.1;
-
-    other.style.opacity -= 0.1;
-
-    if(other.style.opacity == 0)
-    {
-      other.style.display = none;
-      games.style.display = block;
-      games.style.opacity += 0.1;
-    }
-
-    setTimeout(function(){ChangeProjectVisibility(projectType)}, 2000);
-  } 
-  else {
-    clearTimeout(0);
-  }*/
-
-  if(projectType === 'undefined')
-  {
-    localStorage.setItem('projectSelected', 'undefined');
-    document.getElementById("Games").style.display = "none";
-    document.getElementById("Other").style.display = "block";
-    document.getElementById("GameButton").style.color = '#6d6d6d';
-    document.getElementById("OtherButton").style.color = 'white';
-  }
-  else
-  {
-    localStorage.setItem('projectSelected', 'Games');
-    document.getElementById("Games").style.display = "block";
-    document.getElementById("Other").style.display = "none"; 
-    document.getElementById("GameButton").style.color = 'white';
-    document.getElementById("OtherButton").style.color = '#6d6d6d';
-  }
-}
-
+/* #region SlideShow */
 var slideIndex = 1;
 
 // Next/previous controls
@@ -117,3 +83,4 @@ function ShowSlides(n) {
   demoImages[slideIndex-1].className += " Active";
   captionText.innerHTML = demoImages[slideIndex-1].alt;
 }
+/* #endregion */
